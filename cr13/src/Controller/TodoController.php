22a83c70610446157f474c980e9c8bc0c49a7ce5
We-Han <chen007@live.at>
2022-08-06 01:31:48 +0200

    finish

--- cr13/src/Controller/TodoController.php
+++ cr13/src/Controller/TodoController.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +14,7 @@ use App\Form\TodoType;
 
 class TodoController extends AbstractController
 {
-  #[Route('/index', name: 'todo')]
+  #[Route('/', name: 'todo')]
   public function index(ManagerRegistry $doctrine): Response
   {
     $todos = $doctrine->getRepository(Todo::class)->findAll();
@@ -27,27 +29,27 @@ class TodoController extends AbstractController
 
     $form->handleRequest($request);
 
-/* Here we have an if statement, if we click submit and if  the form is valid we will take the values from the form and we will save them in the new variables */
+    /* Here we have an if statement, if we click submit and if  the form is valid we will take the values from the form and we will save them in the new variables */
     if ($form->isSubmitted() && $form->isValid()) {
-        $now = new \DateTime('now');
+      $now = new \DateTime('now');
 
-// taking the data from the inputs with the getData() function and assign it to the $todo variable
-        $todo = $form->getData();
-        // $todo->setCreateDate($now);  // this field is not included in the form so we set the today date
-        $em = $doctrine->getManager();
-        $em->persist($todo);
-        $em->flush();
+      // taking the data from the inputs with the getData() function and assign it to the $todo variable
+      $todo = $form->getData();
+      // $todo->setCreateDate($now);  // this field is not included in the form so we set the today date
+      $em = $doctrine->getManager();
+      $em->persist($todo);
+      $em->flush();
 
-        $this->addFlash(
-            'notice',
-            'Todo Added'
-            );
-    
-        return $this->redirectToRoute('todo');
-      }
+      $this->addFlash(
+        'notice',
+        'Todo Added'
+      );
 
-/* now to make the form we will add this line form->createView() and now you can see the form in create.html.twig file  */
-      return $this->render('todo/create.html.twig', ['form' => $form->createView()]);
+      return $this->redirectToRoute('todo');
+    }
+
+    /* now to make the form we will add this line form->createView() and now you can see the form in create.html.twig file  */
+    return $this->render('todo/create.html.twig', ['form' => $form->createView()]);
   }
 
   #[Route('/edit/{id}', name: 'todo_edit')]
@@ -58,18 +60,18 @@ class TodoController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-        $now = new \DateTime('now');
-        $todo = $form->getData();
-        // $todo->setCreateDate($now);
-        $em = $doctrine->getManager();
-        $em->persist($todo);
-        $em->flush();
-        $this->addFlash(
-             'notice',
-             'Todo Edited'
-             );
+      $now = new \DateTime('now');
+      $todo = $form->getData();
+      // $todo->setCreateDate($now);
+      $em = $doctrine->getManager();
+      $em->persist($todo);
+      $em->flush();
+      $this->addFlash(
+        'notice',
+        'Event Edited'
+      );
 
-        return $this->redirectToRoute('todo');
+      return $this->redirectToRoute('todo');
     }
 
     return $this->render('todo/edit.html.twig', ['form' => $form->createView()]);
@@ -83,13 +85,40 @@ class TodoController extends AbstractController
   }
 
   #[Route('/delete/{id}', name: 'todo_delete')]
-   public function delete($id, ManagerRegistry $doctrine): Response
-   {
-      $todo = $doctrine->getRepository(Todo::class)->find($id);
-      $em = $doctrine->getManager();
-      $em->remove($todo);
-      $em->flush();
-      $this->addFlash("success", "Event has been removed successfully");
-      return $this ->redirectToRoute('todo');
-   }
+  public function delete($id, ManagerRegistry $doctrine): Response
+  {
+    $todo = $doctrine->getRepository(Todo::class)->find($id);
+    $em = $doctrine->getManager();
+    $em->remove($todo);
+    $em->flush();
+    $this->addFlash("success", "Event has been removed successfully");
+    return $this->redirectToRoute('todo');
+  }
+  #[Route('/concert', name: 'concert')]
+  public function concert(ManagerRegistry $doctrine): Response
+  {
+    $todo = $doctrine->getManager();
+    $show = $todo->getRepository(Todo::class)->findBy(['type' => 'Concert']);
+    return $this->render('todo/concert.html.twig', [
+      "todos" => $show
+    ]);
+  }
+  #[Route('/film', name: 'film')]
+  public function film(ManagerRegistry $doctrine): Response
+  {
+    $todo = $doctrine->getManager();
+    $show = $todo->getRepository(Todo::class)->findBy(['type' => 'Film']);
+    return $this->render('todo/film.html.twig', [
+      "todos" => $show
+    ]);
+  }
+  #[Route('/sport', name: 'sport')]
+  public function music(ManagerRegistry $doctrine): Response
+  {
+    $todo = $doctrine->getManager();
+    $show = $todo->getRepository(Todo::class)->findBy(['type' => 'Sport']);
+    return $this->render('todo/sport.html.twig', [
+      "todos" => $show
+    ]);
+  }
 }
--- cr13/src/Form/TodoType.php
+++ cr13/src/Form/TodoType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -22,33 +22,34 @@ class TodoType extends AbstractType
   {
       $builder
           ->add('name', TextType::class, [
-              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px', "placeholder"=>"Event Name"]
           ])
           ->add('date', DateTimeType::class, [
-              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px']
           ])
           ->add('description', TextType::class, [
-              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px', "placeholder"=>"Event Description"]
           ])
           ->add('picture', TextType::class, [
               'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
           ])
           ->add('capacity', IntegerType::class, [
-              'attr' => ['style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px', "placeholder"=>"Event Capacity"]
           ])
           ->add('contact', TextType::class, [
-              'attr' => ['style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px', "placeholder"=>"Email"]
           ])
           ->add('phone', IntegerType::class, [
-              'attr' => ['style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px', "placeholder"=>"Phone Number"]
           ])
           ->add('location', TextType::class, [
-              'attr' => ['style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px', "placeholder"=>"Location"]
           ])
           ->add('url', TextType::class, [
-              'attr' => ['style' => 'margin-bottom:15px']
+              'attr' => ['style' => 'margin-bottom:15px', "placeholder"=>"Events URL"]
           ])
-          ->add('type', TextType::class, [
+          ->add('type', ChoiceType::class, [
+              'choices' => ['Concert' => 'Concert', 'Sport' => 'Sport', 'Film' => 'Film'],
               'attr' => ['style' => 'margin-bottom:15px']
           ])
           ->add('save', SubmitType::class, [
